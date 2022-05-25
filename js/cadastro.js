1f148614a7440f69aa1a867cdbcc57343a3d50b5
Karol-Portela <69825924+Karol-Portela@users.noreply.github.com>
2022-05-24 22:35:44 -0300

    publicação

---
+++ js/cadastro.js
@@ -0,0 +1,47 @@
+// Endereço da API json-server para Candidatos
+let urlCandidatos = "http://localhost:3000/candidatos";
+
+// Selecionando os campos no HTML
+const formulario = document.querySelector("form");
+const campoNome = document.querySelector("#nome");
+const campoData = document.querySelector("#data");
+const campoTelefone = document.querySelector("#telefone");
+const campoEmail = document.querySelector("#email");
+const campoVaga = document.querySelector("#vaga");
+
+formulario.addEventListener("submit", function(){
+    event.preventDefault();
+
+    // Montando um objeto com os dados do formulário
+    let dados = {
+        nome: campoNome.value,
+        data: campoData.value,
+        telefone: campoTelefone.value,
+        email: campoEmail.value,
+
+        // Instrução abaixo permite pegar o título da vaga escolhida
+        vaga: campoVaga.selectedOptions[0].textContent
+    };
+    /* Transmissão dos dados para a API (Técnica Ajax) */
+    fetch(urlCandidatos, {
+        // ENVIO DE DADOS
+        method: "POST", 
+
+        // Converter os dados em JSON
+        body: JSON.stringify(dados), 
+
+        // Cabeçalho da mensagem de dados
+        headers: { // importante p/ back-end api
+            'Content-type' : 'application/json'
+        }
+    })
+    .then(resposta => resposta.json())
+    .then(dados => {
+        console.log(dados);
+        alert("Dados enviados com sucesso!");
+    });
+
+});
+
+// Ativação da mascara para o telefone
+$(campoTelefone).mask("(00) 00000-0000");
